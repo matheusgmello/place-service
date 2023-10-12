d67b412970ec635e4f08c55faacfa4f42194eb33
Matheus Gabriel <matheusgmello1@gmail.com>
2023-10-11 21:28:38 -0300

    Feat: placeController config

---
+++ src/main/java/br/com/matheusgmello/placeservice/web/PlaceController.java
@@ -0,0 +1,31 @@
+package br.com.matheusgmello.placeservice.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.matheusgmello.placeservice.domain.Place;
+import br.com.matheusgmello.placeservice.domain.PlaceService;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+
+  private PlaceService placeService;
+  
+
+  public PlaceController(PlaceService placeService) {
+    this.placeService = placeService;
+  }
+
+
+  @PostMapping
+  public ResponseEntity<Mono<Place>> create(Place place){
+    var createdPlace = placeService.create(place);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+  }
+
+}
